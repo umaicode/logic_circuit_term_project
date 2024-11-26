b3496bebd3de1c9b34d4b9bba8726527b6f2db8b
umaicode <park.seungchan99@gmail.com>
2024-11-26 17:01:53 +0900

    watch_4.v, lcd_control.v와 함께 사용할 textlcd 코드

--- textlcd.v
+++ textlcd.v
@@ -1,210 +1,36 @@
-module textlcd(rst, clk, lce_e, lcd_rs, lcd_rw, lcd_data);
+module textlcd(
+    input rst, clk,
+    input [1:0] mode, // 모드 선택: WATCH/ALARM/STOPWATCH
+    output lcd_e, lcd_rs, lcd_rw,
+    output [7:0] lcd_data
+);
 
-input rst, clk;
-output lcd_e, lcd_rs, lcd_rw;
-output [7:0] lcd_data;
+reg [3:0] cnt;
+wire [7:0] lcd_data_wire;
 
-wire lcd_e;
-reg lcd_rs, lcd_rw;
-reg [7:0] lcd_data;
+// lcd_control 인스턴스
+lcd_control u_lcd_control (
+    .mode(mode), 
+    .cnt(cnt),
+    .lcd_data(lcd_data_wire)
+);
 
-reg [2:0] state;
-parameter dalay = 3'b000, function_set = 3'b001, entry_mode = 3'b010, disp_onoff = 3'b011, line1 = 3b'100, line2 = 3'b101, delay_t = 3'b110, clear_disp = 3'b111;
+assign lcd_data = lcd_data_wire;
 
-integer cnt;
-integer cnt_100hz;
-reg clk_100hz;
+// LCD 제어 신호
+assign lcd_e = clk; // LCD Enable 신호
+assign lcd_rs = (cnt == 0) ? 1'b0 : 1'b1; // 첫 명령어는 명령 모드, 나머지는 데이터 모드
+assign lcd_rw = 1'b0; // 항상 Write 모드
 
+// 문자 출력 타이밍 관리
 always @(posedge rst or posedge clk)
 begin
     if (rst)
-        begin
-            cnt_100hz = 0;
-            clk_100hz = 1'b0;
-        end
-    else if (cnt_100hz >= 4)
-        begin
-            cnt_100hz = 0;
-            clk_100hz = ~clk_100hz;
-        end
+        cnt <= 4'd0;
+    else if (cnt < 4'd11) // 최대 12문자 출력
+        cnt <= cnt + 4'd1;
     else
-        cnt_100hz = cnt_100hz + 1;
+        cnt <= 4'd0; // 반복
 end
-
-always @(posedge rst or posedge clk_100hz)
-begin
-    if (rst)
-        state = delay;
-    else
-        begin
-            case (state)
-                delay :
-                    if (cnt == 70) state = function_set;
-                function_set:
-                    if (cnt == 30) state = disp_onoff;
-                disp_onoff :
-                    if (cnt == 30) state = entry_mode;
-                entry_mode :
-                    if (cnt == 30) state = line1;
-                line1 :
-                    if (cnt == 20) state = line2;
-                line2 :
-                    if (cnt == 20) state = delay_t;
-                delay_t :
-                    if (cnt == 400) state = clear_disp;
-                clear_disp :
-                    if (cnt == 200) state = line1;
-                default :
-                    state = delay;
-            endcase
-        end
-end
-
-always @(posedge rst or posedge clk_100hz)
-begin
-    if(rst)
-        begin
-            lcd_rs = 1'b1;
-            lcd_rw = 1'b1;
-            lcd_data = 8'b00000000;
-        end
-    else
-        begin
-            case (state)
-                function_set :
-                    begin
-                        lcd_rs = 1'b0;
-                        lcd_rw = 1'b0;
-                        lcd_data = 8'b00111100;
-                    end
-                disp_onoff :
-                    begin
-                        lcd_rs = 1'b0;
-                        lcd_rw = 1'b0;
-                        lcd_data = 8'b00001100;
-                    end
-                entry_mode :
-                    begin
-                        lcd_rs = 1'b0;
-                        lcd_rw = 1'b0;
-                        lcd_data = 8'b00000110;
-                    end
-                line1 :
-                    begin
-                        lcd_rw = 1'b0;
-
-                        case (cnt)
-                            0 : 
-                                begin
-                                    lcd_rs = 1'b0;
-                                    lcd_data = 8'b10000000;
-                                end
-                            1 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b00100000;
-                                end
-                            2 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01001000; // H
-                                end
-                            3 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01100101; // E
-                                end
-                            4 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01101100; // L
-                                end
-                            5 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01101100; // L
-                                end
-                            6 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01101111; // O
-                                end
-                            7 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01110111; // W;
-                                end
-                            default :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b00100000;
-                                end
-                        endcase
-                    end
-                line2 :
-                    begin
-                        lcd_rw = 1'b0;
-
-                        case (cnt)
-                            0 :
-                                begin
-                                    lcd_rs = 1'b0;
-                                    lcd_data = 8'b11000000;
-                                end
-                            9 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01010111; // W
-                                end
-                            10 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01101111; // O
-                                end
-                            11 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01110010; // R
-                                end
-                            12 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01101100; // L
-                                end
-                            13 :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b01100100; // D
-                                end
-                            default :
-                                begin
-                                    lcd_rs = 1'b1;
-                                    lcd_data = 8'b00100000;
-                                end
-                        endcase
-                    end
-                delay_t :
-                    begin
-                        lcd_rs = 1'b0;
-                        lcd_rw = 1'b0;
-                        lcd_data = 8'b00000010;
-                    end
-                clear_disp :
-                    begin
-                        lcd_rs = 1'b0;
-                        lcd_rw = 1'b0;
-                        lcd_data = 8'b00000001;
-                    end
-                default :
-                    begin
-                        lcd_rs = 1'b1;
-                        lcd_rw = 1'b1;
-                        lcd_data = 8'b00000000;
-                    end
-            endcase
-        end
-end
-
-assign lcd_e = clk_100hz;
 
 endmodule
