3421530bba8b68fc09a5cc58529118d3fb593087
umaicode <park.seungchan99@gmail.com>
2024-12-03 21:20:56 +0900

    textlcd 수정 (clock_divisor와 연결)

--- test2/textlcd.v
+++ test2/textlcd.v
@@ -1,71 +1,39 @@
-module textlcd(rst, clk, lcd_e, lcd_rs, lcd_rw, lcd_data);
+module textlcd(
+    input rst, clk,
+    output lcd_e, lcd_rs, lcd_rw,
+    output reg [7:0] lcd_data
+);
 
-input rst, clk;
-output lcd_e, lcd_rs, lcd_rw;
-output [7:0] lcd_data;
+    wire clk_100hz;
 
-wire lcd_e;
-reg lcd_rs, lcd_rw;
-reg [7:0] lcd_data;
+    clock_divider clk_div (
+        .clk_in(clk),
+        .rst(rst),
+        .clk_out(clk_100hz)
+    );
 
-reg [2:0] state;
-parameter delay = 3'b000,
-        function_set = 3'b001,
-        entry_mode = 3'b010,
-        disp_onoff = 3'b011,
-        line1 = 3'b100,
-        line2 = 3'b101,
-        delay_t = 3'b110,
-        clear_disp = 3'b111;
+    reg [2:0] state;
+    parameter delay = 3'b000, function_set = 3'b001, entry_mode = 3'b010,
+              disp_onoff = 3'b011, line1 = 3'b100, line2 = 3'b101,
+              delay_t = 3'b110, clear_disp = 3'b111;
 
-integer cnt;
-integer cnt_100hz;
-reg clk_100hz;
+    assign lcd_e = clk_100hz;
 
-always @(posedge rst or posedge clk)
-begin
-    if (rst)
-        begin
-            cnt_100hz = 0;
-            clk_100hz = 1'b0;
-        end
-    else if (cnt_100hz >= 4)
-        begin
-            cnt_100hz = 0;
-            clk_100hz = ~clk_100hz;
-        end
-    else
-        cnt_100hz = cnt_100hz + 1;
-end
-
-always @(posedge rst or posedge clk_100hz)
-begin
-    if (rst)
-        state = delay;
-    else
-        begin
+    always @(posedge clk_100hz or posedge rst) begin
+        if (rst) state <= delay;
+        else begin
             case (state)
-                delay :
-                    if (cnt == 70) state = function_set;
-                function_set:
-                    if (cnt == 30) state = disp_onoff;
-                disp_onoff :
-                    if (cnt == 30) state = entry_mode;
-                entry_mode :
-                    if (cnt == 30) state = line1;
-                line1 :
-                    if (cnt == 20) state = line2;
-                line2 :
-                    if (cnt == 20) state = delay_t;
-                delay_t :
-                    if (cnt == 400) state = clear_disp;
-                clear_disp :
-                    if (cnt == 200) state = line1;
-                default :
-                    state = delay;
+                delay: if (cnt == 70) state <= function_set;
+                function_set: if (cnt == 30) state <= disp_onoff;
+                disp_onoff: if (cnt == 30) state <= entry_mode;
+                entry_mode: if (cnt == 30) state <= line1;
+                line1: if (cnt == 20) state <= line2;
+                line2: if (cnt == 20) state <= delay_t;
+                delay_t: if (cnt == 400) state <= clear_disp;
+                clear_disp: if (cnt == 200) state <= line1;
             endcase
         end
-end
+    end
 
 always @(posedge rst or posedge clk_100hz)
 begin
@@ -211,7 +179,5 @@ begin
             endcase
         end
 end
-
-assign lcd_e = clk_100hz;
 
 endmodule
