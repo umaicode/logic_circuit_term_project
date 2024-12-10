0faa81add7e656b8b57742af6baadb0efb4b23cf
umaicode <park.seungchan99@gmail.com>
2024-12-10 20:57:47 +0900

    fix : dip switch로 시간 설정

--- test3/watch_2.v
+++ test3/watch_2.v
@@ -146,26 +146,3 @@ always @(posedge clk or posedge rst) begin
 end
 
 endmodule
-
-// -----------------------------
-// 세그먼트 디코딩 모듈
-// -----------------------------
-module seg_decode(input [3:0] digit, output reg [7:0] seg);
-
-always @(*) begin
-    case (digit)
-        4'd0: seg = 8'b1100_0000;
-        4'd1: seg = 8'b1111_1001;
-        4'd2: seg = 8'b1010_0100;
-        4'd3: seg = 8'b1011_0000;
-        4'd4: seg = 8'b1001_1001;
-        4'd5: seg = 8'b1001_0010;
-        4'd6: seg = 8'b1000_0010;
-        4'd7: seg = 8'b1111_1000;
-        4'd8: seg = 8'b1000_0000;
-        4'd9: seg = 8'b1001_0000;
-        default: seg = 8'b1111_1111; // 모든 세그먼트 끄기
-    endcase
-end
-
-endmodule
